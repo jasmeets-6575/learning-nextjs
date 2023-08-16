092291adb26f34261b4a3a6eb27b6d012ff74d1f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-16 08:11:00 +0530

    fetching data

--- by-code-with-harry/project1/pages/Blog.js
+++ by-code-with-harry/project1/pages/Blog.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
 
+let url = "http://localhost:3000/api/blogs";
+
 const blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((a) => {
+        return a.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -13,6 +27,7 @@ const blog = () => {
           </Link>
           <p>Js is the lang used to design the logic for web</p>
         </div>
+
         <div>
           <h3 className={styles.blogItemh3}>How to Learn JavaScript in 2022</h3>
           <p>Js is the lang used to design the logic for web</p>
--- by-code-with-harry/project1/pages/api/blogs.js
+++ by-code-with-harry/project1/pages/api/blogs.js
@@ -1,8 +1,23 @@
 import * as fs from "fs";
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data);
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let myFile;
+  let allBlogs = [];
+  data.forEach((item) => {
+    myFile = fs.readFileSync("blogdata/" + item, "utf-8");
+    allBlogs.push(JSON.parse(myFile));
   });
-  res.status(200).json(data);
+  res.status(200).json(allBlogs);
+
+  // fs.promises.readdir("blogdata", (err, data) => {
+  //   console.log(data);
+  //   let allBlogs = [];
+  //   data.forEach((item) => {
+  //     fs.readFile("blogdata/" + item, (d) => {
+  //       allBlogs.push(d);
+  //     });
+  //   });
+  // });
+  // res.status(200).json(allBlogs);
 }
