d926a9f2aa26627eb3e8a455430b63752462b5cf
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-16 07:05:40 +0530

    getBlogs

--- by-code-with-harry/project1/pages/api/blogs.js
+++ by-code-with-harry/project1/pages/api/blogs.js
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 
 export default function handler(req, res) {
-  fs.readFile("../../blogdata/howToLearnFlask.json", "utf-8", (err, data) => {
-    console.log("");
+  fs.readdir("blogdata", (err, data) => {
+    console.log(data);
   });
-  res.status(200).json(JSON.parse(data));
+  res.status(200).json(data);
 }
