ab69ff91b145891603b845db649fd90550506986
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-16 09:55:45 +0530

    slug fetching data

--- by-code-with-harry/project1/pages/blogpost/[slug].js
+++ by-code-with-harry/project1/pages/blogpost/[slug].js
@@ -1,20 +1,29 @@
 import { useRouter } from "next/router";
 import styles from "../../styles/BlogPost.module.css";
+import { useEffect, useState } from "react";
 
+let url = "http://localhost:3000/api/getBlogs?slug=";
 const slug = () => {
+  const [blog, setBlog] = useState();
   const router = useRouter();
-  const { slug } = router.query;
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const { slug } = router.query;
+    fetch(`${url}${slug}`)
+      .then((a) => {
+        return a.json();
+      })
+      .then((data) => {
+        setBlog(data);
+      });
+  }, [router.isReady]);
   return (
     <>
       <div className="container">
         <main className={styles.main}>
-          <div>Title of the page is {slug}</div>
-          <hr />
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
-            blanditiis, voluptate animi reprehenderit velit libero sequi
-            necessitatibus? Officia, fugit ea!
-          </p>
+          <h1 className={styles.title}>{blog && blog.title}</h1>
+          <p className={styles.content}>{blog && blog.content}</p>
         </main>
       </div>
     </>
