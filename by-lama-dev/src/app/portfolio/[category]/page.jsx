11908f160d2b066348309dcf82d222ca552bce42
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-22 21:56:56 +0530

    page done

--- by-lama-dev/src/app/portfolio/[category]/page.jsx
+++ by-lama-dev/src/app/portfolio/[category]/page.jsx
@@ -1,5 +1,46 @@
+import React from "react";
 import styles from "./page.module.css";
-const Category = () => {
-  return <div className={styles.container}>Category</div>;
+import Button from "@/components/Button/Button";
+import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (categ) => {
+  const data = items[categ];
+  if (data) {
+    return data;
+  }
+  return notFound();
 };
+
+const Category = ({ params }) => {
+  const data = getData(params.category);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      {data.map((item) => {
+        return (
+          <div className={styles.item} key={item.id}>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.desc}</p>
+              <Button text="See More" url="#" />
+            </div>
+            <div className={styles.imgContainer}>
+              <Image
+                src={item.image}
+                alt=""
+                fill={true}
+                className={styles.img}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default Category;
