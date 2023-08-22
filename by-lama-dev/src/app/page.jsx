d5877b7284f214124596e5b3ffb7aa0ce7ea5ee3
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-22 19:58:25 +0530

    home page

--- by-lama-dev/src/app/page.jsx
+++ by-lama-dev/src/app/page.jsx
@@ -1,6 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "/public/hero.png";
 
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better Design for your digital products.
+        </h1>
+        <p className={styles.description}>
+          Turning your idea into Reality. We bring together the terms from the
+          global tech industry
+        </p>
+      </div>
+      <div className={styles.item}>
+        <Image
+          width={500}
+          height={500}
+          src={Hero}
+          alt="Main Image"
+          className={styles.img}
+        />
+      </div>
+    </div>
+  );
 }
