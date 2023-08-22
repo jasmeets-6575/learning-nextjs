a03601c0f99408284db33d2ec82001f0bc70a75c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-22 20:43:37 +0530

    setting structure

--- by-lama-dev/src/app/portfolio/page.jsx
+++ by-lama-dev/src/app/portfolio/page.jsx
@@ -1,6 +1,22 @@
 import styles from "./page.module.css";
 
 const Portfolio = () => {
-  return <div className={styles.container}> Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose A Gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/applications" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
+
 export default Portfolio;
