566b359c2e1f1773e1d95cc1f99d57d4dbd4cb88
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-22 18:36:26 +0530

    adding footer

--- by-lama-dev/src/components/footer/Footer.jsx
+++ by-lama-dev/src/components/footer/Footer.jsx
@@ -1,4 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>@lamamia, All Rights Reserved</div>
+      <div>
+        <Image src="/1.png" alt="socials" width={15} height={15} />
+        <Image src="/2.png" alt="socials" width={15} height={15} />
+        <Image src="/3.png" alt="socials" width={15} height={15} />
+        <Image src="/4.png" alt="socials" width={15} height={15} />
+      </div>
+    </div>
+  );
 };
+
 export default Footer;
