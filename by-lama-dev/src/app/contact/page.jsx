3c2fcc6d6039a3e46e2b4bad429052e2306e4db0
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-22 19:17:40 +0530

    adding modul;es css

--- by-lama-dev/src/app/contact/page.jsx
+++ by-lama-dev/src/app/contact/page.jsx
@@ -1,4 +1,8 @@
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+
 const Contact = () => {
-  return <div> Contact</div>;
+  return <div className={styles.container}> Contact</div>;
 };
 export default Contact;
--- by-lama-dev/src/app/portfolio/[category]/page.jsx
+++ by-lama-dev/src/app/portfolio/[category]/page.jsx
@@ -1,4 +1,5 @@
+import styles from "./page.module.css";
 const Category = () => {
-  return <div>Category</div>;
+  return <div className={styles.container}>Category</div>;
 };
 export default Category;
