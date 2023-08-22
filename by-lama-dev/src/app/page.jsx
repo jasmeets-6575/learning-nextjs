9f9d20cbffcfad15d085cc9b0a0bfad3f09070f5
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-22 19:59:53 +0530

    adding button component

--- by-lama-dev/src/app/page.jsx
+++ by-lama-dev/src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "/public/hero.png";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   return (
@@ -13,13 +14,14 @@ export default function Home() {
           Turning your idea into Reality. We bring together the terms from the
           global tech industry
         </p>
+        <Button text="See our Works" url="/portfolio" />
       </div>
       <div className={styles.item}>
         <Image
           width={500}
           height={500}
           src={Hero}
-          alt="Main Image"
+          alt=""
           className={styles.img}
         />
       </div>
