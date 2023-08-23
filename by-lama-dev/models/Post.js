6ba05daa0135f15e954781ec6552347aa6af9026
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-23 11:42:37 +0530

    post Schema

---
+++ by-lama-dev/models/Post.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Post || mongoose.model("Post", PostSchema);
