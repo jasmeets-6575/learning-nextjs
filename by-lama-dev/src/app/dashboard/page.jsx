359060b34de91597a8cab1f5719cfc1f3b321c0f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-23 11:21:25 +0530

    adding swr library

--- by-lama-dev/src/app/dashboard/page.jsx
+++ by-lama-dev/src/app/dashboard/page.jsx
@@ -1,39 +1,34 @@
 "use client";
-import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+import useSWR from "swr";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch("http://localhost:3000/api/posts", {
-        cache: "no-store",
-      });
-      if (!res.ok) {
-        setError(true);
-      }
-      const data = await res.json();
-      setData(data);
-      setIsLoading(false);
-    };
-    getData();
-  }, []);
-
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:3000/api/posts",
+    fetcher
+  );
   return <div className={styles.container}> Dashboard</div>;
 };
 
 export default Dashboard;
+
+// const [data, setData] = useState([]);
+// const [error, setError] = useState(false);
+// const [isLoading, setIsLoading] = useState(false);
+
+// useEffect(() => {
+//   const getData = async () => {
+//     setIsLoading(true);
+//     const res = await fetch("http://localhost:3000/api/posts", {
+//       cache: "no-store",
+//     });
+//     if (!res.ok) {
+//       setError(true);
+//     }
+//     const data = await res.json();
+//     setData(data);
+//     setIsLoading(false);
+//   };
+//   getData();
+// }, []);
