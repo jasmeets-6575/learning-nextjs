0c5a280e248020ec876eb27994ea4ae6459d8d9b
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-23 11:14:34 +0530

    fethcing data in dashboard

--- by-lama-dev/src/app/dashboard/page.jsx
+++ by-lama-dev/src/app/dashboard/page.jsx
@@ -1,5 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch("http://localhost:3000/api/posts", {
+        cache: "no-store",
+      });
+      if (!res.ok) {
+        setError(true);
+      }
+      const data = await res.json();
+      setData(data);
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
   return <div className={styles.container}> Dashboard</div>;
 };
+
 export default Dashboard;
