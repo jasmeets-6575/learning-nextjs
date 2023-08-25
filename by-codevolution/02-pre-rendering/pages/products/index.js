97f5b37ba903f56dc0b96481c6a54f33961f4987
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-25 15:49:14 +0530

    adding product page

---
+++ by-codevolution/02-pre-rendering/pages/products/index.js
@@ -0,0 +1,19 @@
+function ProductList({ products }) {
+  return (
+    <>
+      <h1>List of products</h1>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <h2>
+              {product.id} {product.title} {product.price}
+            </h2>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default ProductList;
