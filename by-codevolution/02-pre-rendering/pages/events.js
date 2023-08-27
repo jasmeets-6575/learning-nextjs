68a5094c5692ef4d5590c9e16c005b0628153c21
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-27 16:24:00 +0530

    rendering data

--- by-codevolution/02-pre-rendering/pages/events.js
+++ by-codevolution/02-pre-rendering/pages/events.js
@@ -1,7 +1,35 @@
-const EventList = () => {
-  return <div>EventList</div>;
-};
+import { useRouter } from "next/router";
+import { useState } from "react";
 
+function EventList({ eventList }) {
+  const [events, setEvents] = useState(eventList);
+  const router = useRouter();
+
+  const fetchSportsEvents = async () => {
+    const resp = await fetch(`http://localhost:4000/events?category=sports`);
+    const data = await resp.json();
+    setEvents(data);
+    router.push("/events?category=sports", undefined, { shallow: true });
+  };
+
+  return (
+    <>
+      <button onClick={fetchSportsEvents}>Sports Events</button>
+      <h1>List of Events</h1>
+      {events.map((event) => {
+        return (
+          <div key={event.id}>
+            <h2>
+              {event.id} {event.title} {event.date} | {event.category}
+            </h2>
+            <p>{event.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
 export default EventList;
 
 export async function getServerSideProps(context) {
