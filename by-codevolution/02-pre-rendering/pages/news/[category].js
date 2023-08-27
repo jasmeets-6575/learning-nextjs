af36af3c344899730071e9ae0d8cb6e5c45a148f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-27 13:55:14 +0530

    fetching articles

--- by-codevolution/02-pre-rendering/pages/news/[category].js
+++ by-codevolution/02-pre-rendering/pages/news/[category].js
@@ -1,11 +1,29 @@
-const ArticleListByCategory = () => {
-  return <div>ArticleListByCategory</div>;
+const ArticleListByCategory = ({ articles, category }) => {
+  return (
+    <>
+      <h1>Showing news for category {category}</h1>
+      {articles.map((article) => {
+        return (
+          <div key={article.id}>
+            <h2>
+              {article.id} {article.title}
+            </h2>
+            <p>{article.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default ArticleListByCategory;
 
 export async function getServerSideProps(context) {
   const { params, req, res, query } = context;
+  console.log(query);
+  console.log(req.headers.cookie);
+  res.setHeader("Set-Cookie", ["name=Jagmeet"]);
   const { category } = params;
   const response = await fetch(
     `http://localhost:4000/news?category=${category}`
