d8c155a65b43fa965bbef522d63c1dbb61265d7e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-28 11:50:04 +0530

    scss

--- by-codevolution/04-styling/pages/about.js
+++ by-codevolution/04-styling/pages/about.js
@@ -1,4 +1,5 @@
-import styles from "../styles/About.module.css";
+// import styles from '../styles/About.module.css'
+import styles from "../styles/About.module.scss";
 
 function About() {
   return <h2 className={styles.highlightscss}>About Page</h2>;
--- by-codevolution/04-styling/pages/contact.js
+++ by-codevolution/04-styling/pages/contact.js
@@ -1,7 +1,7 @@
-import styles from "../styles/Contact.module.css";
+// import styles from '../styles/Contact.module.css'
+import styles from "../styles/Contact.module.scss";
 
 function Contact() {
   return <h2 className={styles.highlightscss}>Contact Page</h2>;
 }
 export default Contact;
-s;
