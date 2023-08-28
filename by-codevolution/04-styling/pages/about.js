4de2c953d2ab99e6c626a109f4e23d962a5c12a4
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-28 11:41:54 +0530

    components

--- by-codevolution/04-styling/pages/about.js
+++ by-codevolution/04-styling/pages/about.js
@@ -1,4 +1,6 @@
-const About = () => {
-  return <div>About</div>;
-};
+import styles from "../styles/About.module.css";
+
+function About() {
+  return <h2 className={styles.highlightscss}>About Page</h2>;
+}
 export default About;
--- by-codevolution/04-styling/pages/contact.js
+++ by-codevolution/04-styling/pages/contact.js
@@ -1,4 +1,7 @@
-const Contact = () => {
-  return <div>Contact</div>;
-};
+import styles from "../styles/Contact.module.css";
+
+function Contact() {
+  return <h2 className={styles.highlightscss}>Contact Page</h2>;
+}
 export default Contact;
+s;
