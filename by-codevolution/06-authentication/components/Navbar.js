50fb87baf09a0537b0dd688c0140300b26a9a91e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-08-28 15:26:29 +0530

    adding navbar component

---
+++ by-codevolution/06-authentication/components/Navbar.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/client";
+
+function Navbar() {
+  const [session, loading] = useSession();
+  return (
+    <nav className="header">
+      <h1 className="logo">
+        <a href="#">NextAuth</a>
+      </h1>
+      <ul className={`main-nav ${!session && loading ? "loading" : "loaded"}`}>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+
+        {!loading && !session && (
+          <li>
+            <Link href="/api/auth/signin">Sign In</Link>
+          </li>
+        )}
+        {session && (
+          <li>
+            <Link href="/api/auth/signout">Sign Out</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
