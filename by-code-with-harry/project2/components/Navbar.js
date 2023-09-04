1a62f0639dad3614d8547898f7b1800894ec1036
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-04 12:49:28 +0530

    adding sideCart

--- by-code-with-harry/project2/components/Navbar.js
+++ by-code-with-harry/project2/components/Navbar.js
@@ -50,18 +50,37 @@ const Navbar = () => {
 
       <div
         ref={ref}
-        className="sideCart absolute top-0 right-0 bg-pink-100 p-10 transform transition-transform translate-x-full"
+        className="sideCart w-72 h-full absolute top-0 right-0 bg-pink-100 px-2 py-10 transform transition-transform translate-x-full"
       >
-        <h2 className="font-bold text-xl">Shopping Cart</h2>
+        <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
         <span
           onClick={toggleCart}
           className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"
         >
           <AiFillCloseCircle />
         </span>
-        <ol>
-          <li>Tshirt - Wear the Code</li>
+        <ol className="list-decimal font-semibold">
+          <li>
+            <div className="item flex my-5">
+              <div className="w-2/3 font-semibold">Tshirt - Wear the Code</div>
+              <div className="w-1/3 font-semibold flex items-center justify-center space-x-3">
+                <AiFillMinusCircle className="mx-1 text-lg text-pink-500 cursor-pointer" />
+                <span className="mx-2">1</span>
+                <AiFillMinusCircle className="mx-1 text-lg text-pink-500 cursor-pointer" />
+              </div>
+            </div>
+          </li>
         </ol>
+        <div className="flex mt-5">
+          <Link href="/checkout">
+            <button className="flex mx-auto text-white bg-pink-500 border-0 py-1 px-6 focus:outline-none hover:bg-pink-600 rounded text-sm">
+              Checkout
+            </button>
+          </Link>
+          <button className="flex mx-auto  text-white bg-pink-500 border-0 py-1 px-6 focus:outline-none hover:bg-pink-600 rounded text-sm">
+            Clear Cart
+          </button>
+        </div>
       </div>
     </div>
   );
