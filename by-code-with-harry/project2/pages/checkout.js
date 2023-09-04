4f5b4cace9dbb9761d7ed1bb24a560f698e8a51e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-04 22:00:58 +0530

    adding inputs

--- by-code-with-harry/project2/pages/checkout.js
+++ by-code-with-harry/project2/pages/checkout.js
@@ -1,4 +1,98 @@
-const Checkout = () => {
-  return <div>Checkout</div>;
+const Checkout = ({ cart, clearCart, addToCart, removeFromCart }) => {
+  return (
+    <div className="container m-auto">
+      <h1 className="font-bold text-3xl my-8 text-center">Checkout</h1>
+      <h2 className="font-bold text-xl ">1. Delivery Details</h2>
+      <div className="mx-auto flex my-4">
+        <div className="px-2 w-1/2">
+          <div className=" mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-white rounded border border-pink-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+        <div className="px-2 w-1/2">
+          <div className=" mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-pink-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex my-4">
+        <div className="px-2 w-1/2">
+          <div className=" mb-4">
+            <label htmlFor="phone" className="leading-7 text-sm text-gray-600">
+              Phone
+            </label>
+            <input
+              type="phone"
+              id="phone"
+              name="phone"
+              className="w-full bg-white rounded border border-pink-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+        <div className="px-2 w-1/2">
+          <div className=" mb-4">
+            <label htmlFor="city" className="leading-7 text-sm text-gray-600">
+              City
+            </label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              className="w-full bg-white rounded border border-pink-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex my-5">
+        <div className="px-2 w-1/2">
+          <div className=" mb-4">
+            <label htmlFor="state" className="leading-7 text-sm text-gray-600">
+              State
+            </label>
+            <input
+              type="text"
+              id="state"
+              name="state"
+              className="w-full bg-white rounded border border-pink-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+        <div className="px-2 w-1/2">
+          <div className=" mb-4">
+            <label
+              htmlFor="pincode"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Pincode
+            </label>
+            <input
+              type="text"
+              id="pincode"
+              name="pincode"
+              className="w-full bg-white rounded border border-pink-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Checkout;
