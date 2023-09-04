4f3e3a2f43600ae8d80c318328b1a455eb4ca178
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-04 14:58:16 +0530

    adding cart different functions

--- by-code-with-harry/project2/pages/_app.js
+++ by-code-with-harry/project2/pages/_app.js
@@ -1,8 +1,41 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [cart, setCart] = useState({});
+  const [subTotal, setsubTotal] = useState(0);
+
+  const saveCart = (item) => {
+    localStorage.setItem("cart", item);
+  };
+  const addToCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty + qty;
+    } else {
+      newCart[itemCode] = { qty: 1, price, name, size, variant };
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  };
+  const removeFromCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty - qty;
+    }
+    if (newCart[itemCode].qty <= 0) {
+      delete newCart[itemCode];
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  };
+  const clearCart = () => {
+    setCart({});
+    saveCart({});
+  };
+
   return (
     <>
       <Navbar />
