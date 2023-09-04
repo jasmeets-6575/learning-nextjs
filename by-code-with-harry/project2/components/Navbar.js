6b2b1a14eb42aefd420ee6675d3c988563c92e87
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-04 12:24:45 +0530

    adding toggleCart

--- by-code-with-harry/project2/components/Navbar.js
+++ by-code-with-harry/project2/components/Navbar.js
@@ -10,6 +10,16 @@ import {
 import { MdAccountCircle } from "react-icons/md";
 
 const Navbar = () => {
+  const ref = useRef();
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.add("translate-x-full");
+      ref.current.classList.remove("translate-x-0");
+    }
+  };
   return (
     <div className="flex flex-col md:flex-row md:justify-center items-center py-2 shadow-md">
       <div className="logo mr-auto md:mx-5">
@@ -33,8 +43,25 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="cart absolute right-0 top-4 mx-5">
-        <AiOutlineShoppingCart className="text-xl md:text-2xl" />
+
+      <div onClick={toggleCart} className="cart absolute right-0 top-4 mx-5 ">
+        <AiOutlineShoppingCart className="text-xl md:text-2xl cursor-pointer" />
+      </div>
+
+      <div
+        ref={ref}
+        className="sideCart absolute top-0 right-0 bg-pink-100 p-10 transform transition-transform translate-x-full"
+      >
+        <h2 className="font-bold text-xl">Shopping Cart</h2>
+        <span
+          onClick={toggleCart}
+          className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-500"
+        >
+          <AiFillCloseCircle />
+        </span>
+        <ol>
+          <li>Tshirt - Wear the Code</li>
+        </ol>
       </div>
     </div>
   );
