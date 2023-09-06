eab17a6fb23d3d688321f28dbb21b04ef3a36967
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-06 17:19:43 +0530

    Search results

--- by-sonny-sangha/01-intro/app/search/[searchTerm]/page.tsx
+++ by-sonny-sangha/01-intro/app/search/[searchTerm]/page.tsx
@@ -1,4 +1,43 @@
-const SearchResults = () => {
-  return <div>SearchResults</div>;
+type PageProps = {
+  params: {
+    searchTerm: string;
+  };
+};
+
+type SearchResult = {
+  organic_results: [
+    {
+      position: number;
+      title: string;
+      link: string;
+      thumbnail: string;
+      snippet: string;
+    }
+  ];
+};
+
+const search = async (searchTerm: string) => {
+  const resp = await fetch(
+    `https://serpapi.com/search.json?q=${searchTerm}&spi_key=${process.env.API_KEY}`
+  );
+  const data: SearchResult = await resp.json();
+  return data;
+};
+
+const SearchResults = async ({ params: { searchTerm } }: PageProps) => {
+  const searchResults = await search(searchTerm);
+  return (
+    <div>
+      <p className="text-gray-500 text-sm">You searched for: {searchTerm}</p>
+      <ol className="space-y-5 p-5">
+        {searchResults.organic_results.map((result) => (
+          <li key={result.position} className="list-decimal">
+            <p className="font-bold">{result.title}</p>
+            <p>{result.snippet}</p>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 export default SearchResults;
